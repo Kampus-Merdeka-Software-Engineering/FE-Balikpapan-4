872edc61f9cb48f21811905fe006644717193fe6
ThreeWulan <wtri32445@gmail.com>
2023-09-24 20:35:49 +0700

    Mengedit file

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ document.addEventListener("click", function (e) {
 // Modal Box
 const itemDetailModal = document.querySelector("#item-detail-modal");
 const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+const formModal = document.querySelector("#form-modal");
+const formModalButtons = document.querySelectorAll(".form-button");
 
 itemDetailButtons.forEach((btn) => {
   btn.onclick = (e) => {
@@ -25,8 +27,47 @@ itemDetailButtons.forEach((btn) => {
   };
 });
 
-// Klik Tombol Close
+formModalButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    formModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik Tombol Close Detail Modal
 document.querySelector(".modal .close-icon").onclick = (e) => {
   itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik Kirim
+function sentWhatsapp() {
+  const phoneNumber = "+6285342426949";
+
+  const name = document.querySelector(".name").value;
+  const eMail = document.querySelector(".eMail").value;
+  const title = document.querySelector(".title").value;
+  const sum = document.querySelector(".sum").value;
+  const address = document.querySelector(".address").value;
+  const description = document.querySelector(".description").value;
+
+  const url =
+    "https://wa.me/" +
+    phoneNumber +
+    "?text=" +
+    `Nama : +${name}+%0a` +
+    `E-mail : +${eMail}+%0a` +
+    `Judul Buku : +${title}+%0a` +
+    `Jumlah : +${sum}+%0a` +
+    `Alamat : +${address}+%0a` +
+    `Deskripsi : +${description}+%0a%0a` +
+    "This is an example of send HTML form data to WhatsApp";
+
+  window.open(url, "_blank").focus();
+}
+
+// Klik Tombol Close Kirim
+document.querySelector("#form-modal .close-icon").onclick = (e) => {
+  formModal.style.display = "none";
   e.preventDefault();
 };
